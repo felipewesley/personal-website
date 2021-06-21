3690a12a142af3473e556e48b71b06e08f793f0d
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-21 00:41:17 -0300

    build inicial da aplicacao

---
+++ app/pages-pages-module.js
@@ -0,0 +1,76 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pages-pages-module"],{
+
+/***/ "dgmN":
+/*!***************************************!*\
+  !*** ./src/app/pages/pages.module.ts ***!
+  \***************************************/
+/*! exports provided: PagesModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PagesModule", function() { return PagesModule; });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _pages_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./pages-routing.module */ "viRw");
+/* harmony import */ var _design_design_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../design/design.module */ "VjqO");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+class PagesModule {
+}
+PagesModule.ɵfac = function PagesModule_Factory(t) { return new (t || PagesModule)(); };
+PagesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({ type: PagesModule });
+PagesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({ imports: [[
+            _angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+            _pages_routing_module__WEBPACK_IMPORTED_MODULE_1__["PagesRoutingModule"],
+            _design_design_module__WEBPACK_IMPORTED_MODULE_2__["DesignModule"],
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](PagesModule, { imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+        _pages_routing_module__WEBPACK_IMPORTED_MODULE_1__["PagesRoutingModule"],
+        _design_design_module__WEBPACK_IMPORTED_MODULE_2__["DesignModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "viRw":
+/*!***********************************************!*\
+  !*** ./src/app/pages/pages-routing.module.ts ***!
+  \***********************************************/
+/*! exports provided: PagesRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PagesRoutingModule", function() { return PagesRoutingModule; });
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+const routes = [
+    {
+        path: '',
+        pathMatch: 'prefix',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        loadChildren: () => __webpack_require__.e(/*! import() | pages-home-home-module */ "pages-home-home-module").then(__webpack_require__.bind(null, /*! ../pages/home/home.module */ "99Un")).then(m => m.HomeModule)
+    }
+];
+class PagesRoutingModule {
+}
+PagesRoutingModule.ɵfac = function PagesRoutingModule_Factory(t) { return new (t || PagesRoutingModule)(); };
+PagesRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineNgModule"]({ type: PagesRoutingModule });
+PagesRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector"]({ imports: [[
+            _angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"].forChild(routes),
+        ], _angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](PagesRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pages-pages-module.js.map
