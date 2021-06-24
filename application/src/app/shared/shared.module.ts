01a79303c5d13937eb4bd977f120aff78191cf63
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-06-24 20:47:11 -0300

    pipe para calculo de tempo

--- application/src/app/shared/shared.module.ts
+++ application/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
+
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { UntilTodayPipe } from './pipes/until-today.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     MatSnackBarModule
   ],
-  declarations: [SharedComponent]
+  declarations: [
+    UntilTodayPipe,
+  ]
 })
 export class SharedModule { }
